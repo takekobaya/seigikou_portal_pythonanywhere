383095095029c6f58be5a9929b424e7176aec619
takekobaya <take.kobaya@gmail.com>
2023-04-26 21:22:51 +0900

    DB 変更（mysql⇒sqlite）

--- seigikou_portal_pythonanywhere/settings/development.py
+++ seigikou_portal_pythonanywhere/settings/development.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-"""
+
 DATABASES = {    
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -29,5 +29,5 @@ DATABASES = {
         'POST'  :'3306',
     }    
 }
-
+"""
 AUTH_PASSWORD_VALIDATORS = []
